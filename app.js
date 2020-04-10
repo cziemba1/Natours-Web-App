f87bdb22bf15d2164dd5eb2cffedff23755cd83c
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-04-10 20:04:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 3000;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.use(express.json()); //middleware
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'sucess',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1; //convierte string en numero
+  const tour = tours.find((el) => el.id === id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body); //creo nuevo objeto
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        //201: created
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Update tour right here...>',
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
